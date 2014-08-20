7de063701050f2fde5fac7e44019d30743bf50da
William Rose <wrose@ondeck.com>
2014-08-20 16:56:08 -0400

    make wget ignore credentials

--- modules/projects/manifests/yodlekeys.pp
+++ modules/projects/manifests/yodlekeys.pp
@@ -2,14 +2,16 @@ class projects::yodlekeys {
 	require wget
 
   wget::fetch { "download private yodle ssh key":
-    source      => 'https://dev-git.dev.yodle.com/projects/SUPERLUMINAL/repos/macguffin/browse/roles/node/files/yodle-denv-bootstrap.key?at=9b7f2f392172535d862954c4d86ac03ee5737cea&raw',
-    destination => '${HOME}/.ssh/yodle-denv-bootstrap.key',
-    verbose     => true,
+    source             => 'https://dev-git.dev.yodle.com/projects/SUPERLUMINAL/repos/macguffin/browse/roles/node/files/yodle-denv-bootstrap.key?at=9b7f2f392172535d862954c4d86ac03ee5737cea&raw',
+    destination        => '${HOME}/.ssh/yodle-denv-bootstrap.key',
+    verbose            => true,
+    nocheckcertificate => true,
   }
 
   wget::fetch { "download public yodle ssh key":
-    source      => 'https://dev-git.dev.yodle.com/projects/SUPERLUMINAL/repos/macguffin/browse/roles/node/files/yodle-denv-bootstrap.key.pub?at=cb94d7d0bdbc6ba5ab78a74a1e9c096ea1157a31&raw',
-    destination => '${HOME}/.ssh/yodle-denv-bootstrap.key.pub',
-    verbose     => true,
+    source             => 'https://dev-git.dev.yodle.com/projects/SUPERLUMINAL/repos/macguffin/browse/roles/node/files/yodle-denv-bootstrap.key.pub?at=cb94d7d0bdbc6ba5ab78a74a1e9c096ea1157a31&raw',
+    destination        => '${HOME}/.ssh/yodle-denv-bootstrap.key.pub',
+    verbose            => true,
+    nocheckcertificate => true,
   }
 }
