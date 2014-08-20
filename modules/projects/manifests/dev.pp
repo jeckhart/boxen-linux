197d14e7b3b1cd7d35245be3b6d95e8b1b6a0b72
William Rose <wrose@ondeck.com>
2014-08-20 16:49:16 -0400

    adding yodle-related projects

---
+++ modules/projects/manifests/dev.pp
@@ -0,0 +1,20 @@
+class projects::dev {
+	require projects::yodlecore
+
+	package { "tomcat":
+	  ensure => present,
+	}
+
+	package { "jetty":
+    ensure => present,
+  }
+
+  package { "thrift":
+    ensure => present,
+  }
+
+  package { "selenium-server-standalone":
+    ensure => present,
+  }
+
+}
