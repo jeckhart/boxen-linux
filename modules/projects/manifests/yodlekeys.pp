b520defe94c82369224cd15ee4d878b9ba3da9cb
William Rose <wrose@ondeck.com>
2014-08-21 18:50:48 -0400

    updating versions

--- modules/projects/manifests/yodlekeys.pp
+++ modules/projects/manifests/yodlekeys.pp
@@ -1,28 +1,28 @@
 class projects::yodlekeys {
-	require wget
-	include boxen::config
+  require wget
+  include boxen::config
 
-	if $osfamily=='Darwin' {
-	  include homebrew
-	  package { 'augeas':
-	    ensure => present
-	  }
-	}
-	elsif $osfamily=='Debian' {
+  if $osfamily=='Darwin' {
+    include homebrew
+    package { 'augeas':
+      ensure => present
+    }
+  }
+  elsif $osfamily=='Debian' {
     include apt
     package { "libaugeas-ruby1.9.1": 
       provider => apt,
     }
-	}
+  }
 
-	$home = "/Users/${::boxen_user}"
+  $home = "/Users/${::boxen_user}"
   $sshdir = "$home/.ssh"
 
-	file { $sshdir:
-	  ensure => directory,
-	  owner  => $::boxen_user,
-	  mode   => 0700
-	}
+  file { $sshdir:
+    ensure => directory,
+    owner  => $::boxen_user,
+    mode   => 0700
+  }
 
   wget::fetch { "download private yodle ssh key":
     source             => 'https://dev-git.dev.yodle.com/projects/SUPERLUMINAL/repos/macguffin/browse/roles/node/files/yodle-denv-bootstrap.key?at=9b7f2f392172535d862954c4d86ac03ee5737cea&raw',
@@ -31,11 +31,11 @@ class projects::yodlekeys {
     nocheckcertificate => true,
   }
 
-	file { "$sshdir/yodle-denv-bootstrap":
-	  ensure => file,
-	  owner  => $::boxen_user,
-	  mode   => 0600
-	}
+  file { "$sshdir/yodle-denv-bootstrap":
+    ensure => file,
+    owner  => $::boxen_user,
+    mode   => 0600
+  }
 
   wget::fetch { "download public yodle ssh key":
     source             => 'https://dev-git.dev.yodle.com/projects/SUPERLUMINAL/repos/macguffin/browse/roles/node/files/yodle-denv-bootstrap.key.pub?at=cb94d7d0bdbc6ba5ab78a74a1e9c096ea1157a31&raw',
@@ -44,13 +44,13 @@ class projects::yodlekeys {
     nocheckcertificate => true,
   }
 
-	file { "$sshdir/yodle-denv-bootstrap.pub":
-	  ensure => file,
-	  owner  => $::boxen_user,
-	  mode   => 0644
-	}
+  file { "$sshdir/yodle-denv-bootstrap.pub":
+    ensure => file,
+    owner  => $::boxen_user,
+    mode   => 0644
+  }
 
-	augeas { "$sshdir/config hg":
+  augeas { "$sshdir/config hg":
     incl    => "$sshdir/config",
     lens    => 'Ssh.lns',
     changes => [
