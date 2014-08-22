131b1d02898d4a6826fb6b68a284a428f209ea13
William Rose <wrose@ondeck.com>
2014-08-21 20:54:42 -0400

    not paying attn to what im writing, awesome

--- modules/projects/manifests/yodlecore.pp
+++ modules/projects/manifests/yodlecore.pp
@@ -1,25 +1,25 @@
 class projects::yodlecore {
-	require projects::yodlekeys
-	include vcsrepo
+  require projects::yodlekeys
+  include vcsrepo
 
-	if $osfamily=='Darwin'
-	  $group = 'staff'
-  else
+  if $osfamily=='Darwin' {
+    $group = 'staff'
+  } else {
     $group = $::boxen_user
-  end
+  }
 
-	file { 'workspace':
-	  path => '/workspace',
-	  owner => $::boxen_user,
+  file { 'workspace':
+    path => '/workspace',
+    owner => $::boxen_user,
     group => $group,
     ensure => directory,
     before => vcsrepo['yodlecore']
-	}
-	
-	vcsrepo {
-		'yodlecore':
-			source   => 'http://dev-hg.dev.yodle.com/yodlecore-central',
-			provider => 'mercurial',
-			path     => '/workspace/src/yodlecore';
-	}
+  }
+  
+  vcsrepo {
+    'yodlecore':
+      source   => 'http://dev-hg.dev.yodle.com/yodlecore-central',
+      provider => 'mercurial',
+      path     => '/workspace/src/yodlecore';
+  }
 }
